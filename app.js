2757244c7b15f4e5c7063d44e2dac08817ebb249
aditya <aaditya1392@gmail.com>
2023-04-16 12:33:10 +0530

    cookies

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express()
 
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL,"http://localhost:5173"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials:true
 }))
